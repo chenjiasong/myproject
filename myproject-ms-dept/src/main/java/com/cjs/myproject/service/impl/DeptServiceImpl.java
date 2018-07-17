774623719c9854fd7a554c1c51fb27aa5e0a9a4c
604613587@qq.com <604613587@qq.com>
2018-07-17 17:53:16 +0800

    提交代码

---
+++ myproject-ms-dept/src/main/java/com/cjs/myproject/service/impl/DeptServiceImpl.java
@@ -0,0 +1,35 @@
+package com.cjs.myproject.service.impl;
+
+import com.cjs.myproject.dao.DeptDao;
+import com.cjs.myproject.entities.Dept;
+import com.cjs.myproject.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService
+{
+	@Autowired
+	private DeptDao dao;
+	
+	@Override
+	public boolean add(Dept dept)
+	{
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id)
+	{
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list()
+	{
+		return dao.findAll();
+	}
+
+}
