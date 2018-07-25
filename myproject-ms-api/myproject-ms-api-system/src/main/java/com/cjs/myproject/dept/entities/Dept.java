c98043c47324a9169639b9b8b68cf745c9ce8284
604613587@qq.com <604613587@qq.com>
2018-07-25 17:55:29 +0800

    增加类注释

--- myproject-ms-api/myproject-ms-api-system/src/main/java/com/cjs/myproject/dept/entities/Dept.java
+++ myproject-ms-api/myproject-ms-api-system/src/main/java/com/cjs/myproject/dept/entities/Dept.java
@@ -11,6 +11,9 @@
 package com.cjs.myproject.dept.entities;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 
 /**
@@ -21,11 +24,15 @@ import java.io.Serializable;
  * @create 2018/7/2
  * @since 1.0.0
  */
+@ApiModel(value="dept对象",description="dept对象")
 public class Dept implements Serializable {
 
-    private Long 	deptno; // 主键
-    private String 	dname; // 部门名称
-    private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+    @ApiModelProperty(value="主键",required=false)
+    private Long 	deptno;
+    @ApiModelProperty(value="部门名称",required=true)
+    private String 	dname;
+    @ApiModelProperty(value="数据库",required=false)
+    private String 	db_source;
 
     public Dept() {
     }
